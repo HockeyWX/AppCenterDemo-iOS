c618dcd3a7f44b59af1ab7c1006ecc41b81fc8e9
Kevin Li <v-lchunx@microsoft.com>
2018-03-23 05:10:50 +0000

    Updated appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,13 +3,12 @@ echo "username: $username"
 password=$PASSWORD
 echo "password: $PASSWORD"
 
-CURL="curl -X GET 'https://api.appcenter.ms/v0.1/user' -H 'accept:application/json' -H 'X-API-Token:4069c5031d6b9a6054a3d16b7f3bdaafb2997fe5'"
-
 #cd $APPCENTER_SOURCE_DIRECTORY
 #echo "build id:"
 #echo $APPCENTER_BUILD_ID
-#export PYTHONIOENCODING=utf8
-#API_RESPONSE=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/KevinApps/AppCenterDemo-iOS/recent_releases" -H "accept: application/json" -H "X-API-Token: 4069c5031d6b9a6054a3d16b7f3bdaafb2997fe5")
+export PYTHONIOENCODING=utf8
+API_RESPONSE=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/KevinApps/AppCenterDemo-iOS/recent_releases" -H "accept: application/json" -H "X-API-Token: 4069c5031d6b9a6054a3d16b7f3bdaafb2997fe5")
+echo $API_RESPONSE
 #lastReleaseID=$( echo $API_RESPONSE | python -c "import json,sys;lst=json.load(sys.stdin);print max([x['id'] for x in lst]);")
 #echo $lastReleaseID
 #newReleaseID=$((lastReleaseID+"1"))
