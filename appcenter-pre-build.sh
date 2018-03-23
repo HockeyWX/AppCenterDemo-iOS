1777f076316bc78d2bec9c88ccf5cd98e8e041b1
Kevin Li <v-lchunx@microsoft.com>
2018-03-23 02:28:10 +0000

    Added appcenter-pre-build.sh

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,16 @@
+username=$USERNAME
+echo "username: $username"
+password=$PASSWORD
+echo "password: $PASSWORD"
+
+#cd $APPCENTER_SOURCE_DIRECTORY
+#echo "build id:"
+#echo $APPCENTER_BUILD_ID
+#export PYTHONIOENCODING=utf8
+#API_RESPONSE=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/KevinApps/AppCenterDemo-iOS/recent_releases" -H "accept: application/json" -H "X-API-Token: 4069c5031d6b9a6054a3d16b7f3bdaafb2997fe5")
+#lastReleaseID=$( echo $API_RESPONSE | python -c "import json,sys;lst=json.load(sys.stdin);print max([x['id'] for x in lst]);")
+#echo $lastReleaseID
+#newReleaseID=$((lastReleaseID+"1"))
+#echo $newReleaseID
+#/usr/bin/find . -name *Info.plist -exec /usr/libexec/PlistBuddy -c Set :CFBundleVersion $newReleaseID {} ;
+#exit 0
